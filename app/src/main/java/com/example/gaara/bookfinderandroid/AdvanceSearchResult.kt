50c2585749a9613314b0d3d882108adc045df184
Ng Wei Shyang <15033996@imail.sunway.edu.my>
2017-10-31 15:48:13 +0800

    Advance Search Result Added

---
+++ app/src/main/java/com/example/gaara/bookfinderandroid/AdvanceSearchResult.kt
@@ -0,0 +1,47 @@
+package com.example.gaara.bookfinderandroid
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gaara.bookfinderandroid.Fragment.BookFragment
+import kotlinx.android.synthetic.main.activity_advance_search_result.*
+
+class AdvanceSearchResult : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advance_search_result)
+        setSupportActionBar(toolbar)
+
+        val books = arrayOf(
+                arrayOf("Intelligent Investor", "Benjamin Graham"),
+                arrayOf("Rich Dad Poor Dad", "Robery Kiyosaki"),
+                arrayOf("The Better Angels of Our Nature", "Steven Pinker"),
+                arrayOf("The Art Of The Deal", "Donald Trump"),
+                arrayOf("The Rational Optimist", "Matt Ridley"),
+                arrayOf("World Order", "Henry Kissinger"))
+
+        listView_Books.adapter = BookFragment(applicationContext, books)
+        listView_Books.divider = this.getDrawable(R.drawable.transparent)
+
+        listView_Books.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this, BookInfo::class.java)
+            intent.putExtra("name", books[position])
+            startActivity(intent)
+        }
+
+        textView_SortBy.setOnClickListener {
+            when(textView_SortBy.text.toString()){
+                "Sort By: Name" -> {
+                    textView_SortBy.text = "Sort By: Price"
+                }
+                "Sort By: Price" -> {
+                    textView_SortBy.text = "Sort By: Author"
+                }
+                "Sort By: Author" -> {
+                    textView_SortBy.text = "Sort By: Name"
+                }
+            }
+        }
+    }
+}
