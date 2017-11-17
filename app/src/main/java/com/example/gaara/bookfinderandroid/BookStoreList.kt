55f78fe7164151910c40437a681e68ea57a32a56
Ng Wei Shyang <15033996@imail.sunway.edu.my>
2017-11-17 21:49:34 +0800

    Added back button and searchview in bookstore

--- app/src/main/java/com/example/gaara/bookfinderandroid/BookStoreList.kt
+++ app/src/main/java/com/example/gaara/bookfinderandroid/BookStoreList.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.example.gaara.bookfinderandroid.Fragment.BookStoreRecycle
 import com.example.gaara.bookfinderandroid.Utils.BookStore
 import kotlinx.android.synthetic.main.activity_book_store_list.*
@@ -35,4 +37,21 @@ class BookStoreList : AppCompatActivity() {
             Log.d("Recycle", "${it.name} is working")
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return true
+    }
 }
+
+
+
+
